f0ce3f8e66d0e3b02420e2fb9efabc2645784e35
Thales Fernandes <thales.fernandes47@gmail.com>
2024-07-03 18:04:48 -0300

    Commit 9 - Criando Arquivos

--- index.js
+++ index.js
@@ -1,85 +1,9 @@
 const { readFileSync } = require('fs');
+var Repositorio = require("./repositorio.js");
+const { formatarMoeda } = require('./utils.js');
+var ServicoCalculoFatura = require("./servico.js") ;
+const {gerarFaturaStr} = require("./apresentacao.js");
 
-class Repositorio {
-  constructor() {
-    this.pecas = JSON.parse(readFileSync('./pecas.json'));
-  }
-
-  getPeca(apre) {
-    return this.pecas[apre.id];
-  }
-}
-
-class ServicoCalculoFatura {
-  constructor(repo) {
-    this.repo = repo;
-  }
-
-  calcularCredito(apre) {
-    let creditos = 0;
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (this.repo.getPeca(apre).tipo === "comedia") {
-      creditos += Math.floor(apre.audiencia / 5);
-    }
-    return creditos;
-  }
-
-  calcularTotalCreditos(apresentacoes) {
-    let creditos = 0;
-    for (let apre of apresentacoes) {
-      creditos += this.calcularCredito(apre);
-    }
-    return creditos;
-  }
-
-  calcularTotalApresentacao(apre) {
-    let total = 0;
-    switch (this.repo.getPeca(apre).tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-          total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-        throw new Error(`Peça desconhecida: ${this.repo.getPeca(apre).tipo}`);
-    }
-    return total;
-  }
-
-  calcularTotalFatura(apresentacoes) {
-    let totalFatura = 0;
-    for (let apre of apresentacoes) {
-      totalFatura += this.calcularTotalApresentacao(apre);
-    }
-    return totalFatura;
-  }
-}
-
-function formatarMoeda(valor) {
-  return new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-    minimumFractionDigits: 2
-  }).format(valor / 100);
-}
-
-function gerarFaturaStr(fatura, calc) {
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
-  }
-  faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
-  faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} \n`;
-  return faturaStr;
-}
 
 // Comente a função gerarFaturaHTML e sua chamada
 // function gerarFaturaHTML(fatura, calc) {
